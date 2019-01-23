16e078976148a37d57f8db4c87d19eaa821fa74a
thanu <thanojkumar7@gmail.com>
2019-01-24 00:29:22 +0530

    python project

---
+++ txt.py
@@ -0,0 +1,14 @@
+{
+    "name": "Python: Current File (Integrated Terminal)",
+    "type": "python",
+    "request": "launch",
+    "program": "${file}",
+    "console": "integratedTerminal"
+},
+{
+    "name": "Python: Current File (External Terminal)",
+    "type": "python",
+    "request": "launch",
+    "program": "${file}",
+    "console": "externalTerminal"
+}
